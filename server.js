0869038e177d58313441171d5e907a448bab4b14
Brandon <bppsparton@gmail.com>
2017-04-01 01:33:12 +0000

    timestamp working now

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+'use strict'
+
 var express = require('express')
+var moment = require('moment')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/:timestamp', (req,res) => {
+  let time = moment(req.params.timestamp, 'MMMM DD, YYYY', true);
+  if (!time.isValid())
+    time = moment.unix(req.params.timestamp);
+  
+  if (!time.isValid()) {
+    res.json({
+      'humanReadable': null,
+      'unix': null
+    });
+  }
+  
+  res.json({
+    'humanReadable': time.format('MMMM DD, YYYY'),
+    'unix': time.format('X')
+  });
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
