a5cc76c1128fbc3fa1c3bbc46c94dd4a28022516
Brandon <bppsparton@gmail.com>
2017-04-05 04:06:11 +0000

    fixed port for heroku

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.get('/:timestamp', (req,res) => {
   });
 });
 
-app.listen(8080, function () {
+ app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port 8080!')
 })
